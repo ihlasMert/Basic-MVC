002cc0b1d6ac3e21ca0fbba5e166b109803e608b
İhlas Mert <ihlas1mert@gmail.com>
2021-12-08 11:27:40 +0300

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by ihlas on 8.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
